26fa0f95bf985c6df2dafb693859faa166cc4e4e
Bahram Movlanov <movlan@users.noreply.github.com>
2020-11-18 12:28:25 -0700

    end of section 13

--- task-manager/src/models/task.js
+++ task-manager/src/models/task.js
@@ -1,21 +1,28 @@
 const mongoose = require("mongoose");
 
-const Task = mongoose.model("Task", {
-  description: {
-    type: String,
-    trim: true,
-    required: true,
-    minlength: 3,
+const taskSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+      minlength: 3,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
   },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "User",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+const Task = mongoose.model("Task", taskSchema);
 
 module.exports = Task;
